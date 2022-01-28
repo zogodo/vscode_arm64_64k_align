1c0bd4935f84ff244e748ac75528fba44d37808c
zogodo <742782908@qq.com>
2022-01-28 17:34:52 +0800

    add struct Elf64_Phdr

--- main.c
+++ main.c
@@ -7,25 +7,25 @@
 
 #define EI_NIDENT 16
 
-#define ElfN_Addr       uint64_t
-#define ElfN_Off        uint64_t
-#define ElfN_Section    uint16_t
-#define ElfN_Versym     uint16_t
+#define Elf64_Addr      uint64_t
+#define Elf64_Off       uint64_t
+#define Elf64_Section   uint16_t
+#define Elf64_Versym    uint16_t
 #define Elf_Byte        unsigned char
-#define ElfN_Half       uint16_t
-#define ElfN_Sword      int32_t
-#define ElfN_Word       uint32_t
-#define ElfN_Sxword     int64_t
-#define ElfN_Xword      uint64_t
+#define Elf64_Half      uint16_t
+#define Elf64_Sword     int32_t
+#define Elf64_Word      uint32_t
+#define Elf64_Sxword    int64_t
+#define Elf64_Xword     uint64_t
 
 typedef struct {
     unsigned char e_ident[EI_NIDENT];
     uint16_t      e_type;
     uint16_t      e_machine;
     uint32_t      e_version;
-    ElfN_Addr     e_entry;
-    ElfN_Off      e_phoff;
-    ElfN_Off      e_shoff;
+    Elf64_Addr    e_entry;
+    Elf64_Off     e_phoff;
+    Elf64_Off     e_shoff;
     uint32_t      e_flags;
     uint16_t      e_ehsize;
     uint16_t      e_phentsize;
@@ -34,6 +34,17 @@ typedef struct {
     uint16_t      e_shnum;
     uint16_t      e_shstrndx;
 } ElfN_Ehdr;
+
+typedef struct {
+    uint32_t   p_type;
+    uint32_t   p_flags;
+    Elf64_Off  p_offset;
+    Elf64_Addr p_vaddr;
+    Elf64_Addr p_paddr;
+    uint64_t   p_filesz;
+    uint64_t   p_memsz;
+    uint64_t   p_align;
+} Elf64_Phdr;
 
 int main()
 {
@@ -48,7 +59,7 @@ int main()
     long filelen = ftell(fileptr);        // Get the current byte offset in the file
     rewind(fileptr);                      // Jump back to the beginning of the file
 
-    char *buffer = (char *)malloc(filelen * sizeof(char)); // Enough memory for the file
+    char* buffer = (char* )malloc(filelen * sizeof(char)); // Enough memory for the file
     fread(buffer, filelen, 1, fileptr);   // Read in the entire file
     fclose(fileptr);                      // Close the file
 
